e2369485ad545331495f69c1ae33b256c734d8c5
belu82 <44766864+belu82@users.noreply.github.com>
2019-10-28 11:45:52 +0100

    Add project files.

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.palyground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.palyground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // palyground
+            // 
+            this.palyground.Controls.Add(this.label3);
+            this.palyground.Controls.Add(this.label1);
+            this.palyground.Controls.Add(this.ball);
+            this.palyground.Controls.Add(this.racket);
+            this.palyground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palyground.Location = new System.Drawing.Point(0, 0);
+            this.palyground.Name = "palyground";
+            this.palyground.Size = new System.Drawing.Size(800, 450);
+            this.palyground.TabIndex = 2;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(164, 155);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.racket.Location = new System.Drawing.Point(219, 354);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(206, 27);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(755, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(685, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SCORE: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.palyground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.palyground.ResumeLayout(false);
+            this.palyground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel palyground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 40;
+        public int speed_top = 40;
+        public int speed_point = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide(); //hide the cruzor
+
+            this.FormBorderStyle = FormBorderStyle.None; //remove any border
+            this.TopMost = true; //bring the from to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            racket.Top = palyground.Bottom - (palyground.Bottom / 10); //beállítája a labdát
+
+
+        }
+
+   
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //quit
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+            //pause 
+            if(e.KeyCode == Keys.Space)
+            {
+                if(timer1.Enabled == true)
+                {
+                    timer1.Stop();
+                }else if(timer1.Enabled == false)
+                {
+                    timer1.Start();
+                }
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (ball.Width / 2);
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+            if(ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom
+                && ball.Right >= racket.Left && ball.Right <= racket.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top; //change direction
+                speed_point += 1;
+                label1.Text = speed_point.ToString();
+            }
+            if(ball.Left <= palyground.Left)
+            {
+                speed_left = -speed_left;
+            }
+
+            if(ball.Right >= palyground.Right)
+            {
+                speed_left = -speed_left;
+            }
+
+            if(ball.Top <= palyground.Top)
+            {
+                speed_top = -speed_top;
+
+            }
+
+            if(ball.Bottom >= palyground.Bottom)
+            {
+                timer1.Enabled = false;
+            }
+        }
+    }
+}
